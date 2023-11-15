c52632d07e9b16ea9495019caa6484f6318ead10
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-11-15 11:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    // Check if the year is a leap year
+    // Leap year is divisible by 4, but century years must be divisible by 400
+    const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+    // Return the number of days based on leap year or not
+    return isLeapYear ? 366 : 365;
+}
+
+// Examples
+console.log(daysOfAYear(2022));  // Output: 365
+console.log(daysOfAYear(2024));  // Output: 366
+console.log(daysOfAYear(1900));  // Output: 365
+console.log(daysOfAYear(2000));  // Output: 366
